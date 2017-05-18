3e3ddabe51893164f238c6c18d5d79fe965cd563
xiaoqigithub <522586087@qq.com>
2017-05-18 18:23:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+	html,body{width: 100%;height: 100%;}
+	.page{width: 100%;height: 100%;}
+</style>
